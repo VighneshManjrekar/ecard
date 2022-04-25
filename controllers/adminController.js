12e0289b0cbaa078944757b9da2f63f3d1c949a1
Vighnesh Manjrekar <vighneshmanjrekar4193@gmail.com>
2022-04-25 18:11:13 +0530

    Add reset doc

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,5 @@
 const { initializeApp } = require("firebase/app");
-const {doc,updateDoc,getFirestore} = require("firebase/firestore")
+const { doc, updateDoc, getFirestore } = require("firebase/firestore")
 initializeApp({
   apiKey: "AIzaSyB3rdSdpboFLC0YcMo2XSNKW-mMyw6vdL4",
   authDomain: "ecard-72ddc.firebaseapp.com",
@@ -11,38 +11,61 @@ initializeApp({
 const db = getFirestore()
 const User = require("../model/userModel")
 
-const getMax = async (arr, cat) => {
-    let obj
-    let max = 0
-    arr.forEach(a => {
-      if (a.max > max) {
-        max = a.max
-        obj = a
-      }
-    })
-    if (obj) {
-        console.log(obj)
-      const winRef = doc(db, "ecard", cat);
-      const winnerUser = await User.findById(obj._id)
-      await updateDoc(winRef,{
-        userName: winnerUser.userName,
-        userMail: winnerUser.userMail,
-        votes:obj.max
+const resetDocs = async () => {
+  try {
+    const users = await User.find()
+    users.forEach(async user => {
+      let category = ["thankYou", "wellDone", "excellence", "attitude", "leader"]
+      user.cardsUsed = 0
+      user.cardsUsed = []
+      category.forEach(cat => {
+        user.cardsRecieved[`${cat}`].qty = 0
+        user.cardsRecieved[`${cat}`].sentBy = []
       })
-    }
-    console.log("ok")
+      user.cardSent = []
+      console.log('ok')
+      await user.save()
+    })
+  } catch (err) {
+    const error = new Error(err);
+    error.httpStatusCode = 500;
+    return next(error);
   }
 
-exports.updateMax = () => {
-    let category = ["thankYou", "wellDone", "excellence", "attitude", "leader"]
-    category.forEach(async cat => {
-      let catWinnerArr = await User.aggregate([
-        {
-          $group: {
-            _id: "$_id",
-            max: { $max: `$cardsRecieved.${cat}.qty` }
-          }
-        }])
-      getMax(catWinnerArr, cat)
+}
+
+const getMax = async (arr, cat) => {
+  let obj
+  let max = 0
+  arr.forEach(a => {
+    if (a.max > max) {
+      max = a.max
+      obj = a
+    }
+  })
+  if (obj) {
+    console.log(obj)
+    const winRef = doc(db, "ecard", cat);
+    const winnerUser = await User.findById(obj._id)
+    await updateDoc(winRef, {
+      userName: winnerUser.userName,
+      userMail: winnerUser.userMail,
+      votes: obj.max
     })
   }
+  resetDocs()
+}
+
+exports.updateMax = () => {
+  let category = ["thankYou", "wellDone", "excellence", "attitude", "leader"]
+  category.forEach(async cat => {
+    let catWinnerArr = await User.aggregate([
+      {
+        $group: {
+          _id: "$_id",
+          max: { $max: `$cardsRecieved.${cat}.qty` }
+        }
+      }])
+    getMax(catWinnerArr, cat)
+  })
+}
